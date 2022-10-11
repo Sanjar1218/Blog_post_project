9c420b01b42e7969d166d9a7f54c5143311f2430
sanjarbek <sanjarsaidov173@gmail.com>
2022-10-11 18:26:21 +0500

    static files

--- databaseApp/views.py
+++ databaseApp/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import User, Post
 
 def home(request):
-    data = User.objects.all()
+    data = User.objects.first().post.all()  # type: ignore
     return render(request, 'pages/home.html', context={'data': data})
