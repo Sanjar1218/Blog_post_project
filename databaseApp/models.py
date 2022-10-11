d355509552ab7aa73e3e82a774b33081d757f206
sanjarbek <sanjarsaidov173@gmail.com>
2022-10-11 09:55:20 +0500

    creating models

--- databaseApp/models.py
+++ databaseApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+
+class Post(models.Model):
+    name = models.CharField(max_length=100) 
+    text = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
