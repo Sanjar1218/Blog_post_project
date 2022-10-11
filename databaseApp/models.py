3e63567e35220b7fc2ca4258aa319fe838375b5f
sanjarbek <sanjarsaidov173@gmail.com>
2022-10-11 10:11:36 +0500

    init html

--- databaseApp/models.py
+++ databaseApp/models.py
@@ -7,4 +7,5 @@ class User(models.Model):
 class Post(models.Model):
     name = models.CharField(max_length=100) 
     text = models.TextField()
+    create_time = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
